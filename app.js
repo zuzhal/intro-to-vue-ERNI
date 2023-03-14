c81838dc873bf06e56ccadcbd47fff2cea0437c0
zuzana.halgasova <zuzana.halgasova@betterask.erni>
2023-03-14 09:52:34 +0100

    added methods

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ Vue.createApp({
     // list of goals
     // input data
     data() {
-        let enteredValue = '';
-        let goals = [];
-        return { enteredValue, goals }
+        enteredValue = '';
+        goals = [];
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue)
+        }
     }
 }).mount('#app');
